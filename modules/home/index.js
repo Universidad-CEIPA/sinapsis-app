6df120a44e60974a66585efc7f18cf6ce0c1572a
Dalaven Uind <dalaven1996@yahoo.com>
2021-04-04 22:49:05 -0500

    avances animación

--- modules/home/index.js
+++ modules/home/index.js
@@ -8,46 +8,43 @@ define([
 			return {
 				projects: this.$root.user.projects,
 				currentIndex: 0,
-				lastDelta: 0,
+				_lastDelta: 0,
 				dragX: 0,
 				dragProject: null
 			};
 		},
 		methods: {
-			handlePointerUp(e) {
+			handlePointerDown(e) {
 				if (e.changedTouches) e = e.changedTouches[0];
-
 				this._pointerStart = e;
-				this.lastDelta = 0;
-
-
+				this._lastDelta = 0;
 			},
 			handlePointerMove(e) {
 				if (e.changedTouches) e = e.changedTouches[0];
-
-				if (this.lastDelta || Math.abs(this._pointerStart.clientX - e.clientX) > 2) {
+				if (this._lastDelta || Math.abs(this._pointerStart.clientX - e.clientX) > 10) {
 					this.dragX = Math.round(e.clientX - this._pointerStart.clientX);
 
 					let delta = this.dragX < 0 ? 1 : -1;
 
-					if (delta !== this.lastDelta) {
-						this.lastDelta = delta;
+					if (delta !== this._lastDelta) {
+						this._lastDelta = delta;
 
 						let preview = this.currentIndex + delta
-						console.log(preview)
 						if (preview >= 0 && preview <= this.projects.length - 1) {
-
-							this.currentIndex = preview
-							this.dragProject = this.projects[preview]
+							this.dragProject = preview
 						}
 					}
 				}
+				console.log(this.dragProject)
 			},
-			handlePointerDown(e) {
+			handlePointerUp(e) {
 				if (e.changedTouches) e = e.changedTouches[0];
-
-				this.lastDelta = 0;
+				if (Math.abs(this._pointerStart.clientX - e.clientX) > 10 && this.dragProject >= 0 && this.dragProject <= this.projects.length - 1 && this.dragProject !== null) {
+					this.currentIndex = this.dragProject
+				}
+				this._lastDelta = 0;
 				this.dragProject = null;
+
 			},
 			goCourse() {
 				this.$root.currentCourse = this.projects[this.currentIndex];
